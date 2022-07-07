dbaef80f4b30859cef2393c7386498cfe0b65436
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-07-07 11:44:43 +0200

    Reestructuración de la página con Node

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index.ejs', function(req, res) {
+  res.render('index');
+});
+
+router.get('/users.ejs', function(req, res) {
+  res.render('users');
+});
+
+router.get('/consejos_seguridad.ejs', function(req, res) {
+  res.render('consejos_seguridad');
+});
+
+router.get('/normas.ejs', function(req, res) {
+  res.render('normas');
+});
+
+router.get('/form.ejs', function(req, res) {
+  res.render('form');
+});
+
+router.get('/filtro.ejs', function(req, res) {
+  res.render('filtro');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
+});
+
+router.get('/user', function(req, res, next) {
+  res.send('user');
 });
 
 module.exports = router;

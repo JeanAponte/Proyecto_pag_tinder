f3021c2b85b6b5b8d4c6db4828f95f33f9915535
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-07-10 18:33:10 +0200

    Corrección de enlaces a scripts y rutas

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/users.ejs', function(req, res) {
+router.get('/user.ejs', function(req, res) {
   res.render('users');
 });
 

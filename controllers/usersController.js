cc4ea12937f5b22f8a630de80ccc5c544b7d2521
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-07-27 19:49:55 +0200

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,57 +21,57 @@ usersController.login = function(req,res,next){
     res.render('user', {email:email, password:password});
   }
 
-/*Funciones de usuario*/
-userController.obtenerUsuario = function (email){
+// /*Funciones de usuario*/
+// userController.obtenerUsuario = function (email){
 
-  /*Comprobar si existe el usuario con el email pasado como parámetro
-    *findOne es buscar a un único usuario mediante una o unas variable/s que le pasemos.
-    *findOne({La propiedad que requiero de la BD: valor de lo que paso por el parámetro})
-    *exec (finction (err, variable donde se guarda el usuario))
-   */
+//   /*Comprobar si existe el usuario con el email pasado como parámetro
+//     *findOne es buscar a un único usuario mediante una o unas variable/s que le pasemos.
+//     *findOne({La propiedad que requiero de la BD: valor de lo que paso por el parámetro})
+//     *exec (finction (err, variable donde se guarda el usuario))
+//    */
 
-  userModel.findOne({email:email}).exec(function(err, usuario){
-    if ( err ){
-       return false; //No se ha encontrado en la BD
-    }else{
-      return usuario; //Devuelve el usuario
-    }
-  });
-}
+//   userModel.findOne({email:email}).exec(function(err, usuario){
+//     if ( err ){
+//        return false; //No se ha encontrado en la BD
+//     }else{
+//       return usuario; //Devuelve el usuario
+//     }
+//   });
+// }
 
-/* Añadir un usuario a la BD */
+// /* Añadir un usuario a la BD */
 
-userModel.agregarUsuario = function(err,usuario){
+// userModel.agregarUsuario = function(err,usuario){
 
-}
+// }
 
-userController.auth = function(email){
-    //Comprobar en la BD si existe el email. 
+// userController.auth = function(email){
+//     //Comprobar en la BD si existe el email. 
 
-    if (userController.obtenerUsuario){
-      if (req.cookies['nombre-usuario']){
-        res.render('user');
-      }else{
-        res.cookie('nombre-usuario', req.body.nombre, 60000).send("cookie definida");//a la variable res se le asigna una cookie con un nombre, un valor y una fecha de expiración (en ms)
-        //Cookie que dura 60 segundos. Por defecto el tiempo de expiración es un mes.
-      }
+//     if (userController.obtenerUsuario){
+//       if (req.cookies['nombre-usuario']){
+//         res.render('user');
+//       }else{
+//         res.cookie('nombre-usuario', req.body.nombre, 60000).send("cookie definida");//a la variable res se le asigna una cookie con un nombre, un valor y una fecha de expiración (en ms)
+//         //Cookie que dura 60 segundos. Por defecto el tiempo de expiración es un mes.
+//       }
 
-      return true;
+//       return true;
 
-    }else{
-      res.render('error');
-      return false;
-    } 
-}
+//     }else{
+//       res.render('error');
+//       return false;
+//     } 
+// }
 
 
-userController.list = function(){
-    //Buscar usuarios en db (atributos queremos de cada usuario)
-    return []; //Array con todos los usuarios
-}
+// userController.list = function(){
+//     //Buscar usuarios en db (atributos queremos de cada usuario)
+//     return []; //Array con todos los usuarios
+// }
 
-userController.friends = function(id_u){
-    return []; //Array con todos los usuarios
-}
+// userController.friends = function(id_u){
+//     return []; //Array con todos los usuarios
+// }
 
 module.exports = usersController;

80712b2552b7afe107dfb4f9b24499be6de4650b
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-07-28 10:15:37 +0200

    Funciones Crud

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var userModel = require("../model/usuario");
+var userModel = require("../model/Usuario");
 
 /*Creo el controlador*/
 var usersController = {};
@@ -41,7 +41,7 @@ usersController.obtenerUsuario = function (email){
   });
 }
 
-/*Listar usuarios */
+/*----------------Listar usuarios------------------*/
 
 usersController.list = function(req, res){
   User.find().exec(function(err, usuarios){
@@ -51,17 +51,90 @@ usersController.list = function(req, res){
   });
 };
 
+/*---------------Añadir un usuario a la BD----------------*/
+
+usersController.createUser = async (req,res) =>{
+    const user = new Usuario({
+        nombre: 'Laura',
+        edad: '20',
+        ciudad: 'París',
+        pais: 'Francia',
+        aficiones: 'Viajar, Naturaleza, Animales',
+        telefono: '035874578',
+        email: 'laura29@gmail.com',
+        fecha_nacimiento: '12/05/2002',
+        imagen: 'https://cronicaglobal.elespanol.com/uploads/s1/49/64/67/mujer.jpeg',
+        sexo: 'Mujer',
+        orientacion_sexual: 'Heterosexual'
+    });
+
+    user.save();
+
+    res.render('../views/user', {usuarios:user})
+}
+
+/*----------------Borrado de usuarios---------------- */
+
+/*Elimina todos los usuarios que tengan el nombre Laura*/
+usersController.deleteUsers = async (req,res) => { 
+  const result = await userModel.deleteMany({nombre: 'Laura'});
+  console.log(result);
+  res.send('Usuarios eliminados');
+}
+
+/*Elimina el primer dato que coincida con el nombre de Ana*/
+usersController.deleteAnElement = async () => { 
+  const result = await userModel.deleteOne({nombre: 'Ana'});
+  console.log(result);
+  res.send('Usuario eliminado');
+}
+
+/*Busca un usuario en específico y lo elimina*/
+usersController.deleteElement = async (req,res) => { 
+  const result = await userModel.findOneAndDelete({email: 'franCisco@msn.com'});
+  console.log(result);
+  res.send('El usuario ' + userModel.nombre +  ' ha sido eliminado');
+}
+
+/*Busca un usuario por ID y lo elimina*/
+usersController.deleteElementById = async () => { 
+  const result = await userModel.findByIdAndDelete('62d93373534b1a3e7bd0ef79');
+  console.log(result);
+  res.send('El usuario ' + userModel.nombre + ' ha sido eliminado');
+}
+
+
+/*------------Actualizar los datos-----------*/
+/*Hay varias formas*/
+
+/*Función para actualizar un objeto.*/
+usersController.updateUser = async function(){
+  const user = await userModel.updateOne({email: 'evandro77@gmail.com'}, 
+  {telefono: '468957539'});
+  console.log(user);
+  user.save();
+  res.render('./views/user', {usuarios:user})
+ }
+
+/*Otro modo de actualizar los datos sin necesidad del método update*/
+usersController.updateUser2 = async (req,res) =>{
+  const user = await userModel.findOne({email: 'camilad8@gmail.com'});
+  console.log(user);
+  user.sexo = 'Hombre'
+  user.save();
+  res.render('./views/user', {usuarios:user})
+}
+
+usersController.updateUser3 = async () => {
+  const user = await userModel.findOneAndUpdate({nombre: 'Jose'}, 
+  {nombre: 'Nina Simone'},
+  {new: true}) //new:true para que se actualice el dato.
+  console.log(user);
+}
 
 // usersController.list = async function(req,res){
 //    const users = await userModel.find();
 //    res.render('../views/users', {usuarios: users}); 
-// }
-
-
-// /* Añadir un usuario a la BD */
-
-// userModel.agregarUsuario = function(err,usuario){
-
 // }
 
 // userController.auth = function(email){
@@ -81,16 +154,6 @@ usersController.list = function(req, res){
 //       res.render('error');
 //       return false;
 //     } 
-// }
-
-
-// userController.list = function(){
-//     //Buscar usuarios en db (atributos queremos de cada usuario)
-//     return []; //Array con todos los usuarios
-// }
-
-// userController.friends = function(id_u){
-//     return []; //Array con todos los usuarios
 // }
 
 module.exports = usersController;
